0e378e50bce099bd7b3323c72a6f13eb06c720a1
Casey MacGibbon <95232019+caseymacgibbon@users.noreply.github.com>
2022-12-21 18:38:39 -0500

    Add files via upload

---
+++ ICMPtraceroute.py
@@ -0,0 +1,131 @@
+# Attribution: this assignment is based on ICMP Traceroute Lab from Computer Networking: a Top-Down Approach by Jim Kurose and Keith Ross. 
+# It was modified for use in CSC249: Networks at Smith College by R. Jordan Crouser in Fall 2022
+
+from socket import *
+from ICMPpinger import checksum
+import os
+import sys
+import struct
+import time
+import select
+import binascii
+
+ICMP_ECHO_REQUEST = 8
+MAX_HOPS = 30
+TIMEOUT = 2.0
+TRIES = 2
+
+# The packet that we shall send to each router along the path is the ICMP echo
+# request packet, which is exactly what we had used in the ICMP ping exercise.
+# We shall use the same packet that we built in the Ping exercise
+def build_packet():
+    # In the sendOnePing() method of the ICMP Ping exercise, firstly the header of our
+    # packet to be sent was made, secondly the checksum was appended to the header and
+    # then finally the complete packet was sent to the destination.
+
+    #---------------#
+    # Fill in start #
+    #---------------#
+
+        # TODO: Make the header in a similar way to the ping exercise.
+    myChecksum = 0
+    ID = os.getpid() & 0xFFFF
+
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+
+    data = struct.pack("d", time.time())
+
+    myChecksum = checksum(header + data)
+    if sys.platform == 'darwin':
+        myChecksum = socket.htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+
+    #-------------#
+  # Fill in end #
+  #-------------#
+
+  # Don’t send the packet yet , just return the final packet in this function.
+    packet = header + data
+    return packet
+
+def get_route(hostname):
+  timeLeft = TIMEOUT
+  for ttl in range(1,MAX_HOPS):
+      for tries in range(TRIES):
+          destAddr = gethostbyname(hostname)
+
+          #---------------#
+          # Fill in start #
+          #---------------#
+
+              # TODO: Make a raw socket named mySocket
+          icmp = socket.getprotobyname("icmp")
+          mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, icmp)
+
+          #-------------#
+          # Fill in end #
+          #-------------#
+
+          mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
+          mySocket.settimeout(TIMEOUT)
+
+          try:
+              d = build_packet()
+              mySocket.sendto(d, (hostname, 0))
+              t= time.time()
+              startedSelect = time.time()
+              whatReady = select.select([mySocket], [], [], timeLeft)
+              howLongInSelect = (time.time() - startedSelect)
+
+              if whatReady[0] == []: # Timeout
+                  print(" * * * Request timed out.")
+
+              recvPacket, addr = mySocket.recvfrom(1024)
+              timeReceived = time.time()
+              timeLeft = timeLeft - howLongInSelect
+
+              if timeLeft <= 0:
+                  print(" * * * Request timed out.")
+
+          except timeout:
+              continue
+
+          else:
+              #---------------#
+              # Fill in start #
+              #---------------#
+
+                  #TODO: Fetch the icmp type from the IP packet
+              icmpHeader = recvPacket[20:28]
+              types, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+              #-------------#
+              # Fill in end #
+              #-------------#
+              
+              if types == 11:
+                  bytes = struct.calcsize("d")
+                  timeSent = struct.unpack("d", recvPacket[28:28 +bytes])[0]
+                  print(" %d rtt=%.0f ms %s" %(ttl, (timeReceived -t)*1000, addr[0]))
+
+              elif types == 3:
+                  bytes = struct.calcsize("d")
+                  timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                  print(" %d rtt=%.0f ms %s" %(ttl, (timeReceived-t)*1000, addr[0]))
+
+              elif types == 0:
+                  bytes = struct.calcsize("d")
+                  timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                  print(" %d rtt=%.0f ms %s" %(ttl, (timeReceived - timeSent)*1000, addr[0]))
+                  return
+
+              else:
+                  print("error")
+
+              break
+
+          finally:
+              mySocket.close()
+
+get_route("www.google.com")
---
+++ reflection.md
@@ -0,0 +1,63 @@
+Use this file to record your reflection on this assignment and answer the prompts.
+
+What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Test your `ping` and `traceroute` programs on 4 target hosts, each on a different continent and include the output below.
+
+This assignment was really difficult for me because I have only used replit and have limited knowledge of pyCharm, so it took a lot to figure this out. If someone were to take this course in the future, I would advise to start early and look up the things you don't know.
+I also was really confused on what the struct import was and how to use it, so I googled that to figure out the details. 
+
+I pinged bbcnews (www.bbcnews.com) in England and this was the output:
+0.016698837280273438
+0.023100852966308594
+0.024359941482543945
+0.022561073303222656
+0.022889137268066406
+0.02449798583984375
+0.023272275924682617
+
+I pinged a school in China (www.tsinghua.edu.cn) and this was the output:
+0.02546405792236328
+0.024058103561401367
+0.02420210838317871
+0.024119138717651367
+0.022488117218017578
+0.02308797836303711
+0.025415897369384766
+0.024183988571166992
+0.023318052291870117
+0.023832082748413086
+0.022594213485717773
+0.02276301383972168
+
+I pinged a high school in South Africa (www.inanda.org) and this was the output:
+0.017594099044799805
+0.023392915725708008
+0.022832155227661133
+0.02232980728149414
+0.022600889205932617
+0.024232149124145508
+0.023689985275268555
+0.023298025131225586
+0.022748947143554688
+0.022463083267211914
+0.023537158966064453
+
+Lastly, I pinged a news outlet in New Zealand (www.odt.co.nz):
+0.12140703201293945
+0.047737836837768555
+0.05593395233154297
+0.034829139709472656
+0.050920963287353516
+0.050994873046875
+0.03601670265197754
+0.04630899429321289
+0.051763296127319336
+0.022597074508666992
+0.01828908920288086
+
+The news outlet in NZ took the longest, which makes sense because it is the farthest geographically. 
+
+references: https://www.jetbrains.com/help/pycharm/configuring-python-interpreter.html
+https://docs.python.org/3/library/struct.html
+https://stackoverflow.com/questions/36322133/icmp-packet-header-layout
